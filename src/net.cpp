f17fd0e6b8636aa6591c2716e967bc8ae10ecc67
David Hou <david.hou314@gmail.com>
2014-01-23 14:58:43 -0800

    Add initial seednode

--- src/net.cpp
+++ src/net.cpp
@@ -1192,6 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"ultimatumdev.org", "scamseed.ultimatumdev.org"},
     {NULL, NULL}
 };
 
